bba7ce63457fe1f6ad79b274dc7a66d529822f17
forger6969 <saidazim186@gmail.com>
2026-01-20 16:59:43 +0500

    save commit

--- src/Components/ModalPractice.jsx
+++ src/Components/ModalPractice.jsx
@@ -30,7 +30,7 @@ const ModalPractice = ({ isOpen, onClose }) => {
       className="absolute shadow shadow-gray-200 w-100 h-auto max-h-49 overflow-y-auto overflow-x-hidden custom-scrollbar bg-white mt-1.5 rounded-2xl py-4 px-5"
     >
       <div className="flex flex-wrap gap-5  ">
-        <div className="bg-[#fffd6a75] flex items-center justify-between w-42.5 h-17.5 py-1.5  px-4 rounded-lg">
+        <div className="bg-[#fffd6a75] flex flex-wrap items-center justify-between w-42.5 h-17.5 py-1.5  px-4 rounded-lg">
           <img className="w-8.25 rounded-sm" src={jsimg} alt="" />
           <div>
             <p className="text-[13px] font-medium text-red-500 ">
